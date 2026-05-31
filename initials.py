d15e324391d9715e1fc31aa593a863c61699f279
jasongyefour-ctrl <jasongyefour@gmail.com>
2026-05-31 21:56:21 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,27 @@
+#FILE:Initials.py
+#Description:print out my initials
+#Assignment number :2
+#
+#NAME:JASON NANA GYEFOUR
+#STUDENT ID : 2425404071
+#Email : jasongyefour@gmail.com
+#Grader : Augustus
+#
+#ON my honor,Augustus Buckman, this programming assignment is my own work
+#And i have not provided my code to any other student.
 
-def main():
-  pass #code goes here
+print("..JNG")
+print()
+print()
+print("...JJJJJJJJJJ........NNN.....NN.........GGGGGG........")
+print(".........JJJJ........NN.NN...NN........GGG....GG......")
+print(".........JJJJ........NN.NN...NN........GGG............")
+print(".........JJJJ........NN..N...NN........GGG............")
+print(".........JJJJ........NN..NN..NN........GGG..GGGGG.....")
+print("........JJJJ.........NN..NN..NN........GGG.....GG.....")
+print("..JJ...JJJJ.....**...NN...NN.NN...**....GGG....GG...**")
+print("....JJJJ........**...NN.....NNN...**......GGGGG.....**")
 
 
 
-main()
+
